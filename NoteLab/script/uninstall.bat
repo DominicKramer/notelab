a77aae68cf0883eaf3ec0ec6f01988df2ec47c52
kramerd <kramerd@7ad6827f-c82c-0410-aae2-80e1801d169c>
2007-06-08 05:53:05 +0000

    Now the DOS variable %CD% is used to reference the current working directory.

--- NoteLab/script/uninstall.bat
+++ NoteLab/script/uninstall.bat
@@ -7,18 +7,17 @@ REM If this file is corrupted you may not be able to uninstall NoteLab.
 REM If you want to edit this file, it is advised that you first make a backup.
 
 REM First store the installation directory, i.e. the current working directory
-CHDIR > dirname.txt
-SET /p INSTALL_DIR=<dirname.txt
+SET INSTALL_DIR=%CD%
 
 REM Start the Java virtual machine and have it load the uninstaller
 java -cp "%INSTALL_DIR%";"%INSTALL_DIR%/info" noteLab.gui.uninstall.UninstallFrame "%INSTALL_DIR%"
 
 REM Finish up by removing the installation directory
 REM First change to the home directory so the install directory is not in use.
-cd "%HOMEDRIVE%%HOMEPATH%"
+cd %HOMEDRIVE%%HOMEPATH%
 
 REM Then remove the installation directory
-rmdir "%INSTALL_DIR%"
+rmdir %INSTALL_DIR%
 
 REM Reset all of the constructed variables
 set INSTALL_DIR=
