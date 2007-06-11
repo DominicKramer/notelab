0463e4d01e214310d7888ee2b3e91f8dc0f4d9f7
kramerd <kramerd@7ad6827f-c82c-0410-aae2-80e1801d169c>
2007-06-11 00:06:23 +0000

    Changed the version number to 0.1.4rc10.

--- NoteLab/src/noteLab/util/InfoCenter.java
+++ NoteLab/src/noteLab/util/InfoCenter.java
@@ -66,7 +66,7 @@ public class InfoCenter
    private static final String NAME = "NoteLab";
    
    /** The application's version. */
-   private static final String VERSION = "0.1.4rc9";
+   private static final String VERSION = "0.1.4rc10";
    
    /** The file extension for this application's native file. */
    private static final String NATIVE_EXT = ".ntlb";
