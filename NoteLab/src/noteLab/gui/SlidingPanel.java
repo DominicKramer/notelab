e09f89c9359551e8ddb6ab64b2f80a1ab7469f84
kramerd <kramerd@7ad6827f-c82c-0410-aae2-80e1801d169c>
2007-10-07 21:56:43 +0000

    Removed an unused import.

--- NoteLab/src/noteLab/gui/SlidingPanel.java
+++ NoteLab/src/noteLab/gui/SlidingPanel.java
@@ -28,7 +28,6 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
