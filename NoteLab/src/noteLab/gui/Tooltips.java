2b9234d428c0cdb233f84d6faf9111651eb622ea
kramerd <kramerd@7ad6827f-c82c-0410-aae2-80e1801d169c>
2009-05-18 08:18:02 +0000

    This interface contains all of the tooltips used throughout the application.

---
+++ NoteLab/src/noteLab/gui/Tooltips.java
@@ -0,0 +1,104 @@
+/*
+ *  NoteLab:  An advanced note taking application for pen-enabled platforms
+ *  
+ *  Copyright (C) 2009, Dominic Kramer
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *  
+ *  For any questions or comments please contact:  
+ *    Dominic Kramer
+ *    kramerd@iastate.edu
+ */
+
+package noteLab.gui;
+
+public interface Tooltips
+{
+   public static final String NEW = "New";
+   public static final String OPEN = "Open";
+   public static final String SAVE = "Save";
+   public static final String SAVE_AS = "Save As";
+   public static final String EXPORT = "Export";
+   public static final String PRINT = "Print";
+   public static final String UNDO = "Undo";
+   public static final String REDO = "Redo";
+   public static final String ZOOM_OUT = "Zoom Out";
+   public static final String ZOOM_IN = "Zoom In";
+   public static final String ZOOM_100 = "Zoom Standard";
+   public static final String PREVIOUS_PAGE = "Previous Page";
+   public static final String CURRENT_PAGE = "Current Page";
+   public static final String NEXT_PAGE = "Next Page";
+   public static final String NEW_PAGE = "New Page";
+   public static final String REFRESH = "Refresh";
+   public static final String FULLSCREEN = "Fullscreen";
+   
+   public static final String WRITING_MODE = "Writing Mode";
+   public static final String STROKE_EDITING_MODE = "Stroke Editing Mode";
+   public static final String PAGE_EDITING_MODE = "Page Editing Mode";
+   
+   public static final String PEN = "Pen";
+   public static final String ERASER = "Eraser";
+   
+   public static final String PEN_1_SIZE = "First Pen Size";
+   public static final String PEN_2_SIZE = "Second Pen Size";
+   public static final String PEN_3_SIZE = "Third Pen Size";
+   public static final String PEN_1_COLOR = "First Pen Color";
+   public static final String PEN_2_COLOR = "Second Pen Color";
+   public static final String PEN_3_COLOR = "Third Pen Color";
+   
+   public static final String STROKE_SELECT = "Select Stroke";
+   public static final String STROKE_UNSELECT = "Unselect Stroke";
+   public static final String REGION_SELECT = "Select Region";
+   public static final String REGION_UNSELECT = "Unselect Region";
+   public static final String STROKE_SELECT_ALL = "Select All Strokes";
+   public static final String STROKE_UNSELECT_ALL = "Unselect All Strokes";
+   public static final String STROKE_MOVE = "Move Selected Strokes";
+   public static final String STROKE_RESIZE = "Resize Selected Strokes";
+   public static final String STROKE_DELETE = "Delete Selected Strokes";
+   public static final String STROKE_SMOOTH = "Smooth Selected Strokes";
+   public static final String STROKE_COLOR = "Set Color of Selected Strokes";
+   public static final String STROKE_SIZE = "Set Size of Selected Strokes";
+   public static final String STROKE_CUT = "Cut Selected Strokes";
+   public static final String STROKE_COPY = "Copy Selected Strokes";
+   public static final String STROKE_PASTE = "Paste Selected Strokes";
+   
+   public static final String PAGE_ACTIVE_EDITING_TOOL = 
+                                                   "The Active Editing Tool";
+   public static final String PAGE_SELECT = "Select Page";
+   public static final String PAGE_UNSELECT = "Unselect Page";
+   public static final String PAGE_SELECT_ALL = "Select All Pages";
+   public static final String PAGE_UNSELECT_ALL = "Unselect All Pages";
+   public static final String PAGE_TYPE = "Set Type of Selected Pages";
+   public static final String PAGE_COLOR = "Set Color of Selected Pages";
+   public static final String PAGE_DELETE = "Delete Selected Pages";
+   public static final String PAGE_DELETE_STROKES = "Clear Selected Pages";
+   public static final String PAGE_CUT = "Cut Selected Pages";
+   public static final String PAGE_COPY = "Copy Selected Pages";
+   public static final String PAGE_PASTE = "Paste Selected Pages";
+   public static final String PAGE_CANCEL_PASTE = "Cancel Paste Selected Pages";
+   public static final String PAGE_OPEN_IMPORT = "Open Document For Import";
+   public static final String PAGE_CANCEL_IMPORT = "Cancel Import";
+   public static final String PAGE_DROP_IMPORT = "Insert Document From Import";
+   
+   public static final String TYPE_PLAIN_PAPER = "Plain Paper";
+   public static final String TYPE_WIDE_RULED_PAPER = "Wide Ruled Paper";
+   public static final String TYPE_COLLEGE_RULED_PAPER = "College Ruled Paper";
+   public static final String TYPE_GRAPH_PAPER = "Graph Paper";
+   
+   public static final String EXPORT_PDF = "Export PDF";
+   public static final String ANNOTATE_PDF = "Annotate PDF";
+   public static final String SETTTINGS = "Edit Settings";
+   public static final String EXIT = "Exit";
+}
