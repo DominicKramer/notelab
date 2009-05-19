43a8387d55ff1c3413a643f1ffb62198680f0db7
kramerd <kramerd@7ad6827f-c82c-0410-aae2-80e1801d169c>
2009-05-19 17:17:32 +0000

    Initial import.

---
+++ NoteLab/src/noteLab/util/render/QueuedRenderer2D.java
@@ -0,0 +1,277 @@
+/*
+ *  NoteLab:  An advanced note taking application for pen-enabled platforms
+ *  
+ *  Copyright (C) 2009, Dominic Kramer
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *  
+ *  For any questions or comments please contact:  
+ *    Dominic Kramer
+ *    kramerd@iastate.edu
+ */
+
+package noteLab.util.render;
+
+import java.awt.Color;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import noteLab.model.Path;
+import noteLab.model.geom.FloatPoint2D;
+import noteLab.util.geom.Bounded;
+
+public class QueuedRenderer2D extends Renderer2D
+{
+   private Queue<Runnable> commandQueue;
+   private Renderer2D renderer;
+   
+   public QueuedRenderer2D(Renderer2D renderer)
+   {
+      if (renderer == null)
+         throw new NullPointerException();
+      
+      this.renderer = renderer;
+      this.commandQueue = new LinkedList<Runnable>();
+   }
+   
+   public void replay()
+   {
+      Runnable cmmd;
+      do 
+      {
+         cmmd = this.commandQueue.poll();
+         if (cmmd != null)
+            cmmd.run();
+         
+      } while (cmmd != null);
+   }
+   
+   public void setRenderer(Renderer2D renderer)
+   {
+      if (renderer == null)
+         throw new NullPointerException();
+      
+      this.renderer = renderer;
+      this.commandQueue.clear();
+   }
+   
+   private void addCommand(Runnable cmmd)
+   {
+      if (cmmd == null)
+         throw new NullPointerException();
+      
+      this.commandQueue.offer(cmmd);
+   }
+   
+   @Override
+   public void tryRenderBoundingBox(final Bounded bounded)
+   {
+      final Color color = this.renderer.getColor();
+      final float width = this.renderer.getLineWidth();
+      
+      addCommand(new Runnable()
+      {
+         public void run()
+         {
+            renderer.setColor(color);
+            renderer.setLineWidth(width);
+            renderer.tryRenderBoundingBox(bounded);
+         }
+      });
+   }
+   
+   @Override
+   public void beginGroup(final Renderable renderable, final String desc, 
+                          final float xScaleLevel, final float yScaleLevel)
+   {
+      final Color color = this.renderer.getColor();
+      final float width = this.renderer.getLineWidth();
+      
+      addCommand(new Runnable()
+      {
+         public void run()
+         {
+            renderer.setColor(color);
+            renderer.setLineWidth(width);
+            renderer.beginGroup(renderable, desc, xScaleLevel, yScaleLevel);
+         }
+      });
+   }
+   
+   @Override
+   public void endGroup(final Renderable renderable)
+   {
+      final Color color = this.renderer.getColor();
+      final float width = this.renderer.getLineWidth();
+      
+      addCommand(new Runnable()
+      {
+         public void run()
+         {
+            renderer.setColor(color);
+            renderer.setLineWidth(width);
+            renderer.endGroup(renderable);
+         }
+      });
+   }
+   
+   @Override
+   protected void beginGroupImpl(Renderable renderable, String desc,
+                                 float xScaleFactor, float yScaleFactor)
+   {
+      this.renderer.beginGroupImpl(renderable, desc, 
+                                   xScaleFactor, yScaleFactor);
+   }
+   
+   @Override
+   protected void endGroupImpl(Renderable renderable)
+   {
+      this.renderer.endGroupImpl(renderable);
+   }
+   
+   @Override
+   public void drawLine(final FloatPoint2D pt1, final FloatPoint2D pt2)
+   {
+      final Color color = this.renderer.getColor();
+      final float width = this.renderer.getLineWidth();
+      
+      addCommand(new Runnable()
+      {
+         public void run()
+         {
+            renderer.setColor(color);
+            renderer.setLineWidth(width);
+            renderer.drawLine(pt1, pt2);
+         }
+      });
+   }
+   
+   @Override
+   public void drawPath(final Path path)
+   {
+      final Color color = this.renderer.getColor();
+      final float width = this.renderer.getLineWidth();
+      
+      addCommand(new Runnable()
+      {
+         public void run()
+         {
+            renderer.setColor(color);
+            renderer.setLineWidth(width);
+            renderer.drawPath(path);
+         }
+      });
+   }
+   
+   @Override
+   public void drawRectangle(final float x, final float y, 
+                             final float width, final float height)
+   {
+      final Color color = this.renderer.getColor();
+      final float lineWidth = this.renderer.getLineWidth();
+      
+      addCommand(new Runnable()
+      {
+         public void run()
+         {
+            renderer.setColor(color);
+            renderer.setLineWidth(lineWidth);
+            renderer.drawRectangle(x, y, width, height);
+         }
+      });
+   }
+   
+   @Override
+   public void fillRectangle(final float x, final float y, 
+                             final float width, final float height)
+   {
+      final Color color = this.renderer.getColor();
+      final float lineWidth = this.renderer.getLineWidth();
+      
+      addCommand(new Runnable()
+      {
+         public void run()
+         {
+            renderer.setColor(color);
+            renderer.setLineWidth(lineWidth);
+            renderer.fillRectangle(x, y, width, height);
+         }
+      });
+   }
+   
+   @Override
+   public void finish()
+   {
+      final Color color = this.renderer.getColor();
+      final float width = this.renderer.getLineWidth();
+      
+      addCommand(new Runnable()
+      {
+         public void run()
+         {
+            renderer.setColor(color);
+            renderer.setLineWidth(width);
+            renderer.finish();
+         }
+      });
+   }
+   
+   @Override
+   public void translate(final float x, final float y)
+   {
+      final Color color = this.renderer.getColor();
+      final float width = this.renderer.getLineWidth();
+      
+      addCommand(new Runnable()
+      {
+         public void run()
+         {
+            renderer.setColor(color);
+            renderer.setLineWidth(width);
+            renderer.translate(x, y);
+         }
+      });
+   }
+   
+   @Override
+   public float getLineWidth()
+   {
+      return this.renderer.getLineWidth();
+   }
+   
+   @Override
+   public boolean isInClipRegion(Bounded bounded)
+   {
+      return this.renderer.isInClipRegion(bounded);
+   }
+   
+   @Override
+   public void setColor(Color color)
+   {
+      this.renderer.setColor(color);
+   }
+   
+   @Override
+   public Color getColor()
+   {
+      return this.renderer.getColor();
+   }
+   
+   @Override
+   public void setLineWidth(float width)
+   {
+      this.renderer.setLineWidth(width);
+   }
+}
