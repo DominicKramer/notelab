341e3b5f8b0b2302a81a294d2627397de6877be0
kramerd <kramerd@7ad6827f-c82c-0410-aae2-80e1801d169c>
2011-05-02 08:06:15 +0000

    Now the default look and feel doesn't need to be set on the command line via this script since the StartupUtilities class handles setting the default look and feel itself.

--- NoteLab/script/notelab.sh
+++ NoteLab/script/notelab.sh
@@ -27,7 +27,7 @@ INSTALL_DIR=`dirname "$0"`
 cd "${INSTALL_DIR}"
 
 # Start the Java virtual machine with the given VM arguments and instruct it to load NoteLab with NoteLab's arguments
-java ${NOTELAB_VM_ARGS} -DNOTELAB_SETTINGS_FILENAME="${INIT_FILE}" -cp .:./info:./jar/PDFRenderer.jar:./jar/iText.jar:"${CLASSPATH}" noteLab.util.StartupUtilities --lookAndFeel Nimbus ${NOTELAB_ARGS} $*
+java ${NOTELAB_VM_ARGS} -DNOTELAB_SETTINGS_FILENAME="${INIT_FILE}" -cp .:./info:./jar/PDFRenderer.jar:./jar/iText.jar:"${CLASSPATH}" noteLab.util.StartupUtilities ${NOTELAB_ARGS} $*
 
 # The variable ${NOTELAB_VM_ARGS} specifies the initial amount and maximum amount of memory that 
 # should be given to the virtual machine.  If NoteLab fails to start from the invocation of 'java' 
